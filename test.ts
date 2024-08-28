2e1387d44ef7962e8b9a08cf29d350fd0f2c533f
Ryan Lee <ryan@parity.io>
2024-08-28 14:30:27 -0400

    gogo

--- test.ts
+++ test.ts
@@ -7,7 +7,10 @@ const log = fs.openSync('test.log', 'w')
 const logs = (await Bun.file("./test7.log").text()).trim().split("\n")
 const client = createClient(
   withLogsRecorder(
-    (l) => fs.writeSync(log, `${l}\n`),
+    (l) => {
+      console.log(l)
+      fs.writeSync(log, `${l}\n`)
+    },
     logsProvider(logs) 
   )
 );
